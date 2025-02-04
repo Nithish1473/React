d8a62d0785a051dc9b17f82b41c888da37938dd9
Nithish <nithish17012003@gmail.com>
2025-02-04 18:50:59 +0530

    react

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
  
 
-function App() {
-  return (
-    <div className="App">
-      <h1 style={{color:"red"}}>Hi Nithish</h1>
-    </div>
-  );
-}
+// function App() {
+//  let ele = React.createElement("div", {className:"App"})
+// }
+// export default App;
 
-export default App;
+// ! component composition 
+// import React from 'react'
+// import Navbar from './components/Navbar'
+// import Main from './components/Main'
+// import Sidebar1 from './components/Sidebar1'
+// import Sidebar2 from'./components/Sidebar2'
+// import Footer from './components/Footer'
+
+// export const App = () => {
+//   return (
+//     <div className='App'>
+//     <Navbar/>
+//     <Main/>
+//     <div className='sidebar'>
+//         <Sidebar1/>
+//         <Sidebar2/>
+//         </div> 
+//         <Footer/>   
+    
+//     </div>
+//   )
+// }
+// export default App
